c483c93d4d1fc181be9200ef0ffa78fb95b41058
Angelo <angelo.vanrooij@student.fontys.nl>
2020-01-14 16:08:30 +0100

    UEX
    Google survey + Task analysis toegevoegd

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+var competitorpdf = "opdrachten/competitoranalysis.pdf";
+var visualpdf = "opdrachten/Visual Associations.pdf";
+var poster1 = "opdrachten/poster1.pdf";
+var button = document.getElementById("")
+var enquete = "opdrachten/Google Enquete.pdf"
+var taskanalysis = "opdrachten/Task analysis.pdf"
+
+function OpenPdf(buttonid) {
+    if (buttonid == "1")
+        window.open(competitorpdf, "_blank");
+    else if (buttonid == "2") {
+        window.open(visualpdf, "_blank");
+    } else if (buttonid == "3") {
+        window.open(poster1, "_blank");
+    }
+    else if (buttonid == "4") {
+        window.open(enquete, "_blank");
+    }
+    else if (buttonid == "5") {
+        window.open(taskanalysis, "_blank");
+    }
+}
