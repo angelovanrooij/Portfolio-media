05097c45e0327cd14b048e39cfc46c7a6ade17df
Angelo <angelo.vanrooij@student.fontys.nl>
2020-01-15 15:41:30 +0100

    Ptm pagina
    opdrachten uex toegevoegd
    Verder gewerkt aan ptm pagina

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,7 @@ var poster1 = "opdrachten/poster1.pdf";
 var button = document.getElementById("")
 var enquete = "opdrachten/Google Enquete.pdf"
 var taskanalysis = "opdrachten/Task analysis.pdf"
+var interview = "opdrachten/Interview.pdf"
 
 function OpenPdf(buttonid) {
     if (buttonid == "1")
@@ -18,5 +19,8 @@ function OpenPdf(buttonid) {
     }
     else if (buttonid == "5") {
         window.open(taskanalysis, "_blank");
+    }
+    else if (buttonid == "6") {
+        window.open(interview, "_blank");
     }
 }
